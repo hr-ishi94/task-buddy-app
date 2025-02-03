6b47da8d7182c6597e14b44ba1d29581dbabb3f8
hprasad <hrishikeshprasadc@gmail.com>
2025-02-03 10:30:43 +0530

    vite config file updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from ‘vite’
 
+import react from ‘@vitejs/plugin-react’
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+
+plugins: [react()],
+
+define: {
+
+'process.env.VITE_GOOGLE_API_KEY':JSON.stringify(process.env.VITE_GOOGLE_API_KEY),
+
+'process.env.VITE_FIREBASE_AUTH_DOMAIN':JSON.stringify(process.env.VITE_FIREBASE_AUTH_DOMAIN),
+
+'process.env.VITE_FIREBASE_PROJECT_ID':JSON.stringify(process.env.VITE_FIREBASE_PROJECT_ID),
+
+'process.env.VITE_FIREBASE_STORAGE_BUCKET':JSON.stringify(process.env.VITE_FIREBASE_STORAGE_BUCKET),
+
+'process.env.VITE_FIREBASE_MESSAGING_SENDER_ID':JSON.stringify(process.env.VITE_FIREBASE_MESSAGING_SENDER_ID),
+
+'process.env.VITE_FIREBASE_APP_ID':JSON.stringify(process.env.VITE_FIREBASE_APP_ID),
+
+}
+
 })
